d982dca002e7adfc1d5bc996cd5ad217cc52d110
dhyutin <62614350+dhyutin@users.noreply.github.com>
2021-09-24 13:31:49 +0530

    commit 2

--- contactus.php
+++ contactus.php
@@ -2,7 +2,7 @@
 session_start();
     if(!isset($_SESSION['status']))
     {
-        header('Location:index.php');
+        header('Location:login.php');
         exit;
     }
     include('connection.php');
