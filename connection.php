e042c0dd928bb62a3148f0e4172680fcabb98712
dhyutin <62614350+dhyutin@users.noreply.github.com>
2021-09-24 16:16:01 +0530

    please work

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     $host = "us-cdbr-east-04.cleardb.com";  
     $user = "b0331734b14842";  
     $password = 'a8d8958b';  
-    $db_name = "login";  
+    $db_name = "heroku_1761586faccbda9	";  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
