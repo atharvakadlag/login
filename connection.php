9dbb9780390cb2ec46bcf65c6367edb9b4fd518b
dhyutin <62614350+dhyutin@users.noreply.github.com>
2021-09-27 12:46:32 +0530

    f

--- connection.php
+++ connection.php
@@ -1,9 +1,8 @@
 <?php      
-    $host = "us-cdbr-east-04.cleardb.com";  
+    $host = "localhost";  
     $user = "b54d61c78cc5f9";  
     $password = 'd186830e';  
-    $db_name = "
-heroku_08621bccaf9a332";  
+    $db_name = "login";  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
